5d659c195f6f286ac55dd4abcfe8415e30f9a439
Shrividya Gopi <shrividya.gopi@gmail.com>
2025-04-01 19:53:44 -0400

    Completed

--- app.js
+++ app.js
@@ -2,9 +2,32 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
+const firstName = "Shrividya";
+let lastName = "Gopi";
+var age = 40;
+lastName = "Vidya";  
+age = 45;
 
-// Exercise 1
+let language = "JavaScript";
+let createdYear = 2020;
+let isCaseSensitive = true;
 
-// Exercise 2
+console.log(`${language} was first released in ${createdYear}.`); 
 
-// Exercise 3
+let product = "T-shirts";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`${product} in size ${selectedSize} is $${price} with ${stock} in stock.`); 
+
+let title = " Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722 ;
+let bookmark =456;
+let hasRead = true;
+
+console.log(`${title} by ${author} is ${pageCount} page long. Your Last bookMark was page ${bookmark}`); 
